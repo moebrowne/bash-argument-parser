add1aaa88d90a4d7261d7861657da3bc716d4094
MoeBrowne <oliver.browne@uplyme.com>
2015-11-01 17:21:11 +0000

    Set debugging to false by default

--- argument-parser.sh
+++ argument-parser.sh
@@ -8,7 +8,7 @@ regexArgLongWithValue='^--([a-zA-Z0-9\-]{2,})=(.*)$'
 
 argChunks=()
 
-ARG_DEBUG=true
+ARG_DEBUG=false
 ARG_MUST_BE_DEFINED=true
 
 # Expand chained short form arguments, eg -aih => -a -i -h
