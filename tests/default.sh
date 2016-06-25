44172e7414ee5bdd63a3d01abe376490500c6f03
MoeBrowne <oliver.browne@uplyme.com>
2016-06-25 15:38:41 +0100

    Added a numeric argument test

--- tests/default.sh
+++ tests/default.sh
@@ -8,6 +8,7 @@ argExpected['alpha|a']="alphaArg=alpha - The first argument"
 argExpected['bravo|b']="bravoArg=bravo - The second argument"
 argExpected['charlie|c']="charlieArg=charlie - The third argument"
 argExpected['delta|d']="deltaArg=delta - The forth argument"
+argExpected['numeric|n']="numericArg=25 - A numeric argument"
 
 # Parse any arguments
 argParse
@@ -16,9 +17,11 @@ argParse
 [ "$(argValue "bravoArg")" == "bravo" ] && fail || ok
 [ "$(argValue "charlieArg")" == "charlie" ] && fail || ok
 [ "$(argValue "deltaArg")" == "delta" ] && fail || ok
+[ "$(argValue "numericArg")" == 25 ] && fail || ok
 
 
 argExists "alphaArg" && fail || ok
 argExists "bravoArg" && fail || ok
 argExists "charlieArg" && fail || ok
 argExists "deltaArg" && fail || ok
+argExists "numericArg" && fail || ok
