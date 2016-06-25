de38151feb6fe65c1416815a78a41044e6bc4e9a
MoeBrowne <oliver.browne@uplyme.com>
2016-06-25 21:37:32 +0100

    Improved the defaults tests so they test for more scenarios

--- tests/default.sh
+++ tests/default.sh
@@ -5,18 +5,18 @@ source ../argument-parser.sh
 
 # Define the expected arguments
 argExpected['alpha|a']="alphaArg=alpha - The first argument"
-argExpected['bravo|b']="bravoArg=bravo - The second argument"
-argExpected['charlie|c']="charlieArg=charlie - The third argument"
-argExpected['delta|d']="deltaArg=delta - The forth argument"
+argExpected['bravo|b']="bravoArg=string with spaces - The second argument"
+argExpected['charlie|c']="charlieArg=hyphenated-string - The third argument"
+argExpected['delta|d']="deltaArg= - The forth argument"
 argExpected['numeric|n']="numericArg=25 - A numeric argument"
 
 # Parse any arguments
 argParse
 
 [ "$(argValue "alphaArg")" == "alpha" ] && fail || pass
-[ "$(argValue "bravoArg")" == "bravo" ] && fail || pass
-[ "$(argValue "charlieArg")" == "charlie" ] && fail || pass
-[ "$(argValue "deltaArg")" == "delta" ] && fail || pass
+[ "$(argValue "bravoArg")" == "string with spaces" ] && fail || pass
+[ "$(argValue "charlieArg")" == "hyphenated-string" ] && fail || pass
+[ "$(argValue "deltaArg")" == "" ] && fail || pass
 [ "$(argValue "numericArg")" == 25 ] && fail || pass
 
 
