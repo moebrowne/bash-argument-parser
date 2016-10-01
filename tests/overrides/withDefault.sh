0ade3d681108d7769dc1394a8a557c580393c606
MoeBrowne <oliver.browne@uplyme.com>
2016-10-01 01:52:06 +0100

    Added more extensive testing for overrides

---
+++ tests/overrides/withDefault.sh
@@ -0,0 +1,16 @@
+source tools.sh
+
+echo -n '['
+
+# Define the expected arguments
+declare -A argExpected
+argExpected['long|s']="arg=unexpectedDefault - The first argument"
+
+# Include the Argument Parser library
+source ../argument-parser.sh
+
+[ "$(argValue "arg")" == "expected" ] && pass || fail
+
+argPassed "arg" && pass || fail
+
+echo -n '] '
--- tests/run.sh
+++ tests/run.sh
@@ -7,3 +7,19 @@ echo "# Core"
 
 echo "# Defaults"
 ./default/run.sh
+
+echo "# Overrides"
+
+echo -n " - Short Override Long	"
+./overrides/withDefault.sh --long=unexpected -s expected
+./overrides/withDefault.sh --long unexpected -s expected
+./overrides/withoutDefault.sh --long=unexpected -s expected
+./overrides/withoutDefault.sh --long unexpected -s expected
+echo
+
+echo -n " - Long Override Short	"
+./overrides/withDefault.sh -s unexpected --long=expected
+./overrides/withDefault.sh -s unexpected --long expected
+./overrides/withoutDefault.sh -s unexpected --long=expected
+./overrides/withoutDefault.sh -s unexpected --long expected
+echo
